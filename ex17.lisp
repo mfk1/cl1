825e4af85eeaa30006b275968c9b04ca76fa54fe
Manfred Kendlbacher <mfk1@users.noreply.github.com>
2014-10-27 06:11:51 +0100

    Update ex17.lisp

--- ex17.lisp
+++ ex17.lisp
@@ -73,3 +73,17 @@
   (cond ((null l) '())
 	((eql old (first l)) (cons new (subst* new old (rest l))))
 	(t (cons (first l) (subst* new old (rest l))))))
+
+;;;Exercise 17.28
+(defun xprod (s1 s2)
+  (check-type s1 (satisfies set:setp))
+  (check-type s2 (satisfies set:setp))
+  (cons :set (xprodh (rest s1) (rest s2))))
+
+(defun xprodh (s1 s2)
+  (cond ((null s1) '())
+	(t (append (xprod1 (first s1) s2) (xprodh (rest s1) s2)))))
+
+(defun xprod1 (e s)
+  (cond ((null s) '())
+	(t (cons (list e (first s)) (xprod1 e (rest s))))))
