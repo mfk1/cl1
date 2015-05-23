14027d6e270d52c44f3eff3d153a4d3c8e2a5394
Manfred Kendlbacher <mfk1@users.noreply.github.com>
2015-05-23 16:50:06 +0200

    Update calculator.lisp

--- calculator.lisp
+++ calculator.lisp
@@ -3,5 +3,7 @@
 
 ;;; The solution of exercise 12.6
 (defun combine-expr (o1 o2 l)
-  "Takes an arithmetic operator, an operand and a list representing an arithmetic operation and returns the expression with the operator and operand applied to the first member of the expression. (combine-expr ’+ '3 ’(5 - 6 * 8)) should evaluate to ((3 + 5) - 6 * 8)."
+  "Takes an arithmetic operator, an operand and a list representing an arithmetic operation and returns 
+  the expression with the operator and operand applied to the first member of the expression.
+  So (combine-expr ’+ '3 ’(5 - 6 * 8)) should evaluate to ((3 + 5) - 6 * 8)."
   (cons (list o2 o1 (first l)) (rest l)))
