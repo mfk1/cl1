df78a160f20679de3d423d227138ada68e6bc072
Manfred Kendlbacher <mfk1@users.noreply.github.com>
2015-05-23 16:52:32 +0200

    Update match.lisp

--- match.lisp
+++ match.lisp
@@ -1,6 +1,7 @@
 (defpackage match)
 (in-package match)
 
+;;; The solution of exercise 12.2
 (defun variablep (s)
   "Takes a symbol S and returns True if its first charakter is #\?; NIL otherwise"
   (char= (char (symbol-name s) 0) #\?))
