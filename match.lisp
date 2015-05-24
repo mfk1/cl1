c39b975205ea7b6b96c0293db9a32d5ac788a518
Manfred Kendlbacher <mfk1@users.noreply.github.com>
2015-05-24 18:41:06 +0200

    Update match.lisp

--- match.lisp
+++ match.lisp
@@ -5,10 +5,21 @@
 (defun variablep (s)
   "Takes a symbol S and returns True if its first charakter is #\?;
    NIL otherwise"
-  (and (symbolp s) (char= (char (symbol-name s) 0) #\?)))
+  (and (symbolp s) (> (length (symbol-name s)) 1) (char= (char (symbol-name s) 0) #\?)))
 
-;;; Exercise 13.13
+;;; Exercise 13.13 and 14.8
 (defun match-element (e1 e2)
-  "Takes two Lisp objects and returns True if they are eql or if either of them
-is a variable recognized by variablep"
-  (or (eql e1 e2) (or (variablep e1) (variablep e2)))) 
+  "Takes two Lisp objects and returns True if the two arguments are eql or either argument is ?; if either argument is a variable (as recognized by variablep), match-element returns a two-member list whose first member is the variable and whose second member is the other argument; otherwise, match-element returns NIL.
+"
+  (cond ((variablep e1) (cons e1 (cons e2 '())))
+	((variablep e2) (cons e2 (cons e1 '())))
+	(t  (or (eql e1 e2) (eql e1 '?) (eql e2 '?)))))
+
+;;; Exercise 14.7
+(defun dont-care (s)
+  "Returns True if its argument is a question mark symbol; NIL otherwise."
+  (if (not (symbolp s))
+	   NIL
+	   (string= (symbol-name s) "?")))
+;;; other solution (replaces the if expression):
+;;; (and (symbolp s) (string= (symbol-name s) "?"))
