0d03c1e16777973555e618db55824eb3d115087d
Manfred Kendlbacher <mfk1@users.noreply.github.com>
2015-05-24 12:54:14 +0200

    Update match.lisp

--- match.lisp
+++ match.lisp
@@ -1,7 +1,14 @@
 (defpackage match)
 (in-package match)
 
-;;; The solution of exercise 12.2
+;;; Exercises 12.2 and 13.14
 (defun variablep (s)
-  "Takes a symbol S and returns True if its first charakter is #\?; NIL otherwise"
-  (char= (char (symbol-name s) 0) #\?))
+  "Takes a symbol S and returns True if its first charakter is #\?;
+   NIL otherwise"
+  (and (symbolp s) (char= (char (symbol-name s) 0) #\?)))
+
+;;; Exercise 13.13
+(defun match-element (e1 e2)
+  "Takes two Lisp objects and returns True if they are eql or if either of them
+is a variable recognized by variablep"
+  (or (eql e1 e2) (or (variablep e1) (variablep e2)))) 
